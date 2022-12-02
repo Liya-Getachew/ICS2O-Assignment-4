3f00c9cca3f7e3f6365a12da6f058df548eaed37
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2022-12-02 02:46:26 +0000

    new code

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@
 "use strict"
 
 /**
- * Check servie worker.
+ * Check service worker.
  */
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/ICS2O-Assignment-4/sw.js", {
@@ -16,8 +16,45 @@ if (navigator.serviceWorker) {
 }
 
 /**
- * This function displays an alert.
+ * This function calculates the total price of the user's order.
  */
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  const flavour = document.getElementById("flavour").value
+  const size = document.getElementById("size").value
+  const TAX_RATE = 1.13
+  let toppingPrice = 0
+  let sizePrice = 0
+
+  if (flavour == "sprinkles") {
+    toppingPrice = 1.00
+  } else if (flavour == "glazed") {
+    toppingPrice = 2.00
+  } else if (flavour == "jam filling") {
+    toppingPrice = 1.50
+  } else if (flavour == "powdered sugar") {
+    toppingPrice = 1.00
+  } else if (flavour == "caramel") {
+    toppingPrice = 3.00
+  } else {
+    toppingPrice = 0
+  }
+
+  if (size == "one piece") {
+    sizePrice = 1.00
+  } else if (size == "six piece") {
+    sizePrice = 7.00
+  } else {
+    sizePrice = 15.00
+  }
+  
+  document.getElementById("title").innerHTML = "Receipt"
+  document.getElementById("subtitle").innerHTML = "Item ----- Cost"
+  document.getElementById("sizePicked").innerHTML = size + " " + sizePrice.toFixed(2)
+  document.getElementById("toppingPicked").innerHTML = flavour + " " + toppingPrice.toFixed(2)
+  document.getElementById("taxes").innerHTML = "Taxes %13"
+
+  const totalPrice = (sizePrice + toppingPrice) * TAX_RATE
+
+  document.getElementById("total").innerHTML = "Total " + "$" + totalPrice.toFixed(2)
+  document.getElementById("thanks").innerHTML = "Thanks for purchasing!"
 }
